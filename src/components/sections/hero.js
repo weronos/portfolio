34cc0366af19fbafbcf48370577610f1d16a2443
Weronos <weronos.business@gmail.com>
2023-08-05 09:57:41 +1000

    update 1

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Eric Huang.</h2>;
+  const two = <h2 className="big-heading">Heriaue Mello.</h2>;
   const three = <h3 className="big-heading">I build things for the mobile.</h3>;
   const four = (
     <>
